8df9e8ed75666ca8f4d770474f80f5b042fbabab
henriquesd <hsd.foz@gmail.com>
2020-05-31 20:08:12 +0200

    Created Entity and Repository class

---
+++ src/WhaleEcommerce.Domain/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WhaleEcommerce.Domain.Models
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
